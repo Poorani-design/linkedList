c1026175622d5c5de0db3c86689043083220d247
POORANI GUNASEKAREN <94600498+Poorani-design@users.noreply.github.com>
2022-04-17 16:22:44 +0530

    Create linkedlist.c

---
+++ linkedlist.c
@@ -0,0 +1,31 @@
+971434#include <stdio.h>
+#include <stdlib.h>
+struct createNewNode{
+    int eid,ename; //eid
+    struct createNewNode *link; 
+};
+void main()
+{
+    struct createNewNode *head = NULL;
+    head = (struct createNewNode *)malloc(sizeof(struct createNewNode));
+    head->eid=500;
+    head->link = NULL;
+    printf("\n %d",head->eid);
+    
+    struct createNewNode *body=NULL;
+    body=( struct createNewNode *)malloc(sizeof(struct createNewNode));
+    body->eid=600;
+    body->link=NULL;
+    head->link=body;
+    printf("\n %d",body->eid);
+    
+    struct createNewNode *footer=NULL;
+    footer=( struct createNewNode *)malloc(sizeof(struct createNewNode));
+    footer->eid=700;
+    footer->link=NULL;
+    body->link = footer;
+    printf("\n %d",footer->eid);
+}
+
+    
+
